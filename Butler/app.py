82e44adb2e6e7f5a635ae138ce295bdd2a28e84c
Hrishikesh332 <93283437+Hrishikesh332@users.noreply.github.com>
2023-01-02 17:20:12 +0530

    Update app.py

--- Butler/app.py
+++ Butler/app.py
@@ -89,5 +89,24 @@ if (selected=="Reimaginator"):
     st.subheader("Do you want to be more clear while expressing on text?")
     st.write("Reimaginator helps you to convey your thoughts in a clear and concise manner by rephrasing your text. The Reimaginator provides you the new perspective to view the text and helps you to be expressive in a correct way.")
     st.caption("Add your text here to rephrase it, and click Reimagine!!!")
+    statement1=st.text_area("Add your content below!!!")
+    button2=st.button("Summarize")
+    def response3(statement1):
+        openai.api_key = st.secrets["Api"]
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Rephrase the provided statement\nQ:{statement1}\nA: ",
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        print(response)
+        return response.choices[0].text
+    if statement and button2:
+           reimagine = response2(statement1)
+           st.write(summary)
     
 
