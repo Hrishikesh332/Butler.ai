6e42f77cfecb40d5da5813fe30712178d0caf4b4
Jay Oza <71382456+jayoza198@users.noreply.github.com>
2023-01-02 14:56:55 +0530

    Update app.py

--- Butler/app.py
+++ Butler/app.py
@@ -64,6 +64,25 @@ if (selected=="Wizard Summarizer"):
     st.write("Are you bored of reading a long text and wanted to cover long text in a short time or wanted to summarize the long text in the best way. Here, Bulter.ai provides you the wand and the wizard summarizer to help you out.")
     st.caption("Add your long text in the box below! and click the apply spell button")
 
+statement=st.text_area("Add your content below!!!")
+    button=st.button("Summarize")
+    def response1(summ):
+        openai.api_key = st.secrets["Api"]
+
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=f"Summarize anything\nQ:{statement}\nA: ",
+            temperature=0.7,
+            max_tokens=256,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=0
+        )
+        print(response)
+        return response.choices[0].text
+    if statement and button:
+        reply=response1(statement)
+        st.write(reply)
     
 
 if (selected=="Reimaginator"):
