899330bf752fee5d1653f2f543c75fba983da288
Jay Oza <71382456+jayoza198@users.noreply.github.com>
2023-04-22 11:44:59 +0530

    Update app.py

--- Butler/app.py
+++ Butler/app.py
@@ -48,15 +48,6 @@ if (selected=="Intellichat"):
         reply=response1(question)
         st.write(reply)
         
-       
-    
-
-
-
-if (selected=="Quick Caption"):
-    st.subheader("Do you like reading more and taking notes?")
-    st.write("Quick Caption helps you generate the caption/transcript from the video very easily. The transcript generated is concise and clear. The benefits of using the Transcipter are that it saves a lot of time to take out the notes.")
-    st.caption("Add your video url below and click on transcript!!!")
     
 
 if (selected=="Wizard Summarizer"):
